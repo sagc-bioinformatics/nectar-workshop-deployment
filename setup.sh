b47d82d1f862a7dc11b2f54748b94e15600ba94a
John Salamon <salamonj9@gmail.com>
2024-08-19 19:14:54 +0930

    wooo

---
+++ setup.sh
@@ -0,0 +1,33 @@
+# Add Termuin repository
+wget -qO - https://packages.adoptium.net/artifactory/api/gpg/key/public | gpg --dearmor | tee /etc/apt/trusted.gpg.d/adoptium.gpg > /dev/null
+echo "deb https://packages.adoptium.net/artifactory/deb noble main" | tee /etc/apt/sources.list.d/adoptium.list > /dev/null
+
+# Update package lists
+apt update
+
+# Install Termuin
+apt install -y temurin-21-jdk
+
+su - user -c 'bash -s' <<'EOF'
+
+# Install Nextflow
+mkdir -p /home/user/.local/bin && cd /home/user/.local/bin && curl -s https://get.nextflow.io | bash
+nextflow info
+
+# Install conda/mamba (miniforge)
+wget "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
+bash Miniforge3-$(uname)-$(uname -m).sh -b -p /home/user/.miniforge3
+rm Miniforge3-$(uname)-$(uname -m).sh
+cd ~/.local/bin && ln -s ~/.miniforge3/bin/mamba . && ln -s ~/.miniforge3/bin/conda .
+mamba init && conda init && conda config --set auto_activate_base false
+
+# Create snakemake env, and link to the executable
+mamba create -c conda-forge -c bioconda -n snakemake snakemake
+cd ~/.local/bin && ln -s ~/.miniforge3/envs/snakemake/bin/snakemake .
+
+# Clone data
+git clone https://github.com/snakemake/snakemake-tutorial-data.git
+mv snakemake-tutorial-data/data/ .
+rm -rf snakemake-tutorial-data
+
+EOF
